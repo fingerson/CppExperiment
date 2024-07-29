50d56837da09f6faa930b498c8c24e7641ff9ad3
henrique <henfinzim@gmail.com>
2024-07-29 00:01:21 -0300

    crow added to vcpkg, template

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 int main(int argc, char** argv) {
   std::cout<<"Hello, World 2!"<<std::endl;
   crow::SimpleApp app;
+  crow::mustache::set_global_base("src/templates");
 
   CROW_ROUTE(app, "/")([](){
-    return "Hello World!";
+    std::string page = crow::mustache::load_text("fancypage.html");
+    return page;
   });
 
   app.port(18080).multithreaded().run();
